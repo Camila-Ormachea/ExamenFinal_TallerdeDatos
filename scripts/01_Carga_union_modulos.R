8a8ba9f246622a37ed3cfa05f748d440476a2d39
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-07-08 23:39:25 -0500

    Creamos script para la carga de unión de mólulos de ENAHO

---
+++ scripts/01_Carga_union_modulos.R
@@ -0,0 +1,18 @@
+#==============================================================================
+#Proyecto: Análisis usando datos de la ENAHO
+#Autora: Camila Ormachea
+#Objetivo de este script: Cargar los módulos y hacer los joints
+#Fecha: 7 de julio del 2026
+#==============================================================================
+
+#1. Carga de librerias---------------------------------------------------------
+
+library(rio)
+library(tidyverse)
+install.packages("janitor")
+library(janitor)
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
