688871bbc500e41455d6039d6245d12973741ccb
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-07-12 03:07:24 -0500

    Selección y renombre de las variables de interés

--- scripts/02_Acondicionamiento.R
+++ scripts/02_Acondicionamiento.R
@@ -3,8 +3,8 @@
 # Script: Acondicionamiento 
 # Autor: Camila Ormachea 
 # Fecha: 8-07-2026
-# Objetivo: Acondicionar la base de datos consolidada (Tipado, Selección, 
-#           Renombrado, Tratamiento de NAs).
+# Objetivo: Acondicionar la base de datos consolidada mediante la selección, 
+#           renombrado, tipado y diagnóstico de valores perdidos.
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
@@ -15,3 +15,54 @@ library(arrow)
 library(janitor)
 library(naniar)
 renv::snapshot()
+
+# ------------------------------------------------------------------------------
+# 1. CARGA, SELECCIÓN, RENOMBRADO Y DIAGNÓSTICO---------------------------------
+# ------------------------------------------------------------------------------
+# Leemos la base que consolidamos en el script anterior en formato parquet.
+
+enaho_raw <- read_parquet("datos/procesados/enaho_2025_modulos.parquet")
+
+#Selección de las variables de interés para trabajar con una base menos pesada
+#Al mismo tiempo que seleccionamos, renombramos usando las herramientas de dplyr
+
+enaho_seleccion <- enaho_raw %>%
+  select(
+    # Llaves de integración y variables identificadoras 
+    año = AÑO,
+    mes = MES,
+    conglome = CONGLOME, 
+    subconglome = SUB_CONGLOME,
+    vivienda = VIVIENDA, 
+    hogar    = HOGAR, 
+    codperso  = CODPERSO, 
+    ubigeo   = UBIGEO, 
+    dominio  = DOMINIO, 
+    estrato  = ESTRATO, 
+    factorpob07 = FACPOB07,
+    factor07 = FACTOR07,
+
+    #Características demográficas (módulo 200)
+    parentesco = P203,
+    sexo = P207,
+    edad = P208A,
+    estado_civil = P209,
+    
+    #Características de la vivienda (módulo 100)
+    tipo_vivienda = P101,
+    material_pared = P102,
+    ocupacion_vivienda = P105A,
+    
+    #Servicios básicos (módulo 100)
+    abastecimiento_agua = P110,
+    servicio_higienico = P111A,
+    electricidad = P1121,
+    
+    #Necesidades básicas insatisfechas (módulo 100)
+    vivienda_inadecuada = NBI1,
+    hacinamiento = NBI2,
+    hogarsin_servicio_higienico = NBI3,
+    ninos_no_escolarizados = NBI4,
+    alta_dependencia_economica = NBI5
+    
+  )
