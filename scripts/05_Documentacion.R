58dc3296e07b1715269f899ffadb6a4352952f79
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-07-13 00:41:04 -0500

    Se carga la base para su selección en el codebook

--- scripts/05_Documentacion.R
+++ scripts/05_Documentacion.R
@@ -5,3 +5,17 @@
 # Fecha: 12-07-2026
 # Objetivo: Añadir metadatos a la base analítica y generar el codebook final.
 # ==============================================================================
+
+# ------------------------------------------------------------------------------
+# 0. CONFIGURACIÓN Y PAQUETES
+# ------------------------------------------------------------------------------
+install.packages(c("labelled", "codebook"))
+
+library(tidyverse)
+library(arrow)
+library(here)
+library(labelled)# Para inyectar etiquetas y metadatos en las variables
+library(codebook)  # Para automatizar el libro de códigos interactivo
+
+# Cargamos nuestra base de datos con las etiquetas ya creadas (script de Exploración)
+enaho_final <- read_parquet(here("datos", "procesados", "enaho_exploracion.parquet"))
