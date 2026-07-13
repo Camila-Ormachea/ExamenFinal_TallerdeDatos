9ee8887bd46977f13defaad0ed9445cf9bb2683a
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-07-13 00:39:23 -0500

    Configuración inicial del script de Documentación

---
+++ scripts/05_Documentacion.R
@@ -0,0 +1,7 @@
+# ==============================================================================
+# Proyecto: Análisis usando datos de la ENAHO
+# Script: Documentar
+# Autora: Camila Ormachea
+# Fecha: 12-07-2026
+# Objetivo: Añadir metadatos a la base analítica y generar el codebook final.
+# ==============================================================================
