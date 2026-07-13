25276f466fc13a09ec1fc3278a919456f5788ed6
Camila-Ormachea <a20220717@pucp.edu.pe>
2026-07-13 00:43:11 -0500

    Selección de variables para el codebook y exportación de base de datos final

--- scripts/05_Documentacion.R
+++ scripts/05_Documentacion.R
@@ -19,3 +19,27 @@ library(codebook)  # Para automatizar el libro de códigos interactivo
 
 # Cargamos nuestra base de datos con las etiquetas ya creadas (script de Exploración)
 enaho_final <- read_parquet(here("datos", "procesados", "enaho_exploracion.parquet"))
+
+# ==============================================================================
+# 1. SELECCIÓN DE VARIABLES PARA EL CODEBOOK
+# ==============================================================================
+
+# Creamos una base de datos con las variablesusadas en el reporte descriptivo 
+# demografía, vivienda y NBI)
+
+enaho_codebook <- enaho_final %>%
+  select(
+    edad, sexo_etiqueta, parentesco_etiqueta, estado_civil_etiqueta,
+    tipo_vivienda_etiqueta, material_pared_etiqueta, ocupacion_vivienda_etiqueta,
+    abastecimiento_agua_etiqueta, servicio_higienico_etiqueta, electricidad_etiqueta,
+    vivienda_inadecuada_etiqueta, hacinamiento_etiqueta, hogar_sin_sshh_etiqueta,
+    ninos_no_escolarizados_etiqueta, alta_dependencia_etiqueta
+  ) %>%
+  mutate(across(where(is.character), as.factor)) # Para que "Codebook" detecte nuestras etiquetas
+
+# Exportamos como la base de datos final de nuestro proyecto
+write_parquet(enaho_codebook, here("datos", "procesados", "enaho_2025_codebook.parquet"))
+
+
+
+
